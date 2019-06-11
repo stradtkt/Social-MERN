df8ccd4b188b0b8b3e9de0a3de9826f48e8439c3
Kevin <stradtkt22@gmail.com>
2019-06-11 12:40:08 -0400

    Created the start of routes

---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('API Running');
 });
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
