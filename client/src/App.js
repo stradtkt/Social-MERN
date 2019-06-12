3f86573ebae94763766ce6ad1c6736c6d0e05284
Kevin <stradtkt22@gmail.com>
2019-06-12 15:54:45 -0400

    Added structure for a few pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <Route exact={true} path='/' component={Landing}/>
+                <section className="container">
+                    <Switch>
+                        <Route exact={true} path='/login' component={Login}/>
+                        <Route exact={true} path='/register' component={Register}/>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    );
 }
 
 export default App;
