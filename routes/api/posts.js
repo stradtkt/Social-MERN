2eec49c75138b3b8b185bd90b5e8d4c9e46ede16
Kevin <stradtkt22@gmail.com>
2019-06-12 13:50:28 -0400

    Added post a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
 router.get('/', async (req, res) => {
     try {
@@ -9,11 +14,27 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', [
+    auth,
+    check('text', 'Text is required').not().isEmpty()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     try {
-        
+        const user = await User.findById(req.user.id).select('-password');
+        const newPost = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });   
+        const post = await newPost.save();
+        res.json(post);
     } catch (err) {
-        
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 
